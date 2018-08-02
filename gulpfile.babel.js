73051a68fef1831468485b5469ed9e73862097b5
UnnamedHero <hero.nospam@gmail.com>
2018-08-02 09:04:32 +0300

    remove gulp-babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,5 @@
 import gulp from 'gulp';
-import babel from 'gulp-babel';
 import getServer from '.';
-
-gulp.task('deafult', () => {
-  gulp.src('index.js')
-    .pipe(babel({
-      plugins: ['@babel/transform-runtime'],
-    }))
-    .pipe(gulp.dest('dist'));
-});
 
 gulp.task('server', (cb) => {
   getServer().listen(process.env.PORT || 3000, cb);
