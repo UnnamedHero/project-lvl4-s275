064988d1ace6e4b16286b3b2ee3ac4ceb93e064c
UnnamedHero <hero.nospam@gmail.com>
2018-08-04 22:33:14 +0300

    fix sequelize config init

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.js`)[env]; //eslint-disable-line
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(config);
 
 fs
   .readdirSync(__dirname)
