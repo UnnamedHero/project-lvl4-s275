168ac49ba0cc2046fbee725ca7ef6555eec72a97
UnnamedHero <hero.nospam@gmail.com>
2018-08-06 17:45:25 +0300

    add repl task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,19 @@
 import gulp from 'gulp';
+import repl from 'repl';
+
+import container from './src/server/container';
 import getServer from './src/server';
 
 gulp.task('server', (cb) => {
   getServer().listen(process.env.PORT || 3000, cb);
+});
+
+gulp.task('console', () => {
+  const replServer = repl.start({
+    prompt: 'Application console > ',
+  });
+
+  Object.keys(container).forEach((key) => {
+    replServer.context[key] = container[key];
+  });
 });
