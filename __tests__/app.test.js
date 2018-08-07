09819f878f37f55d5589fd01fb9d009c5f4e8915
UnnamedHero <hero.nospam@gmail.com>
2018-08-07 10:29:36 +0300

    add base test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,33 @@
-test('example', () => {
+import request from 'supertest';
+import matchers from 'jest-supertest-matchers';
 
+import app from '../src/server';
+
+describe('requests', () => {
+  let server;
+
+  beforeAll(() => {
+    jasmine.addMatchers(matchers);
+  });
+
+  beforeEach(() => {
+    server = app().listen();
+  });
+
+  it('GET 200', async () => {
+    const res = await request.agent(server)
+      .get('/');
+    expect(res).toHaveHTTPStatus(200);
+  });
+
+  it('GET 404', async () => {
+    const res = await request.agent(server)
+      .get('/wrong-path');
+    expect(res).toHaveHTTPStatus(404);
+  });
+
+  afterEach((done) => {
+    server.close();
+    done();
+  });
 });
