fbe711f9b384ac3a06bae0a96eb4a1fcd233d3ad
UnnamedHero <hero.nospam@gmail.com>
2018-08-14 11:23:07 +0300

    rename helper

--- src/lib/formErrorObjectBuilder.js
+++ src/lib/formErrorObjectBuilder.js
@@ -1,6 +1,6 @@
 export const hasErrors = errors => Object.keys(errors).length > 0;
 
-export const makeErrors = errObject => ({
+export const makeErrorsObj = errObject => ({
   errors: Object.keys(errObject)
     .map(err => ({
       path: err,
