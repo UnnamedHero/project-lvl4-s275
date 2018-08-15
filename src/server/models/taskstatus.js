569e21d46deeae0f5eac07c462c4e77f2b223647
UnnamedHero <hero.nospam@gmail.com>
2018-08-15 18:28:55 +0300

    add task status model

---
+++ src/server/models/taskstatus.js
@@ -0,0 +1,12 @@
+export default (sequelize, DataTypes) => {
+  const TaskStatus = sequelize.define('TaskStatus', {
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+  });
+  // TaskStatus.associate = function(models) {
+    // associations can be defined here
+  // };
+  return TaskStatus;
+};
