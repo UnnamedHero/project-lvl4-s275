07fc31713ab1c0e2ca022d61e0c733b02febef9c
UnnamedHero <hero.nospam@gmail.com>
2018-08-17 17:23:30 +0300

    add tag model

---
+++ src/server/models/tag.js
@@ -0,0 +1,17 @@
+export default (sequelize, DataTypes) => {
+  const Tag = sequelize.define('Tag', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  }, {});
+
+  Tag.associate = (models) => {
+    Tag.belongsToMany(models.Task, { through: 'TaskTags' });
+  };
+
+  return Tag;
+};
