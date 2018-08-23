fd0b4964c4dd5fff815753944360d20504665552
UnnamedHero <hero.nospam@gmail.com>
2018-08-23 17:09:24 +0300

    add tags helper lib

---
+++ src/lib/models/tags.js
@@ -0,0 +1,27 @@
+import uniq from 'lodash/uniq';
+import { Tag } from '../../server/models'; //eslint-disable-line
+
+const getTagsFromString = str => uniq(str
+  .split(' ')
+  .map(tagName => tagName.trim())
+  .filter(tag => tag));
+
+const getOrCreateTags = async (tagNames) => {
+  const iter = async (acc, tags) => {
+    const [tagName, ...rest] = tags;
+    if (!tagName) {
+      return acc;
+    }
+
+    const [result] = await Tag.findOrCreate({ where: { name: tagName } });
+    return iter([...acc, result], rest);
+  };
+  const tags = await iter([], tagNames);
+  return tags;
+};
+
+export default async (stringOfTags) => {
+  const tagNames = getTagsFromString(stringOfTags);
+  const tags = await getOrCreateTags(tagNames);
+  return tags;
+};
